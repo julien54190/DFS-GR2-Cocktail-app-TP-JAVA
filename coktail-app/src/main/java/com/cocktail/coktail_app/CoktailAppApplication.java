b5b518dca3d9a4445a805cb06b65e684422d47c5
julien54190 <lucadello.julien@hotmail.fr>
2025-08-05 13:41:33 +0200

    initialisation du projet

---
+++ coktail-app/src/main/java/com/cocktail/coktail_app/CoktailAppApplication.java
@@ -0,0 +1,13 @@
+package com.cocktail.coktail_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoktailAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoktailAppApplication.class, args);
+	}
+
+}
---
+++ coktail-app/src/test/java/com/cocktail/coktail_app/CoktailAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cocktail.coktail_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoktailAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
